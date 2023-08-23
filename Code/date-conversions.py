0e6f9ae1415d69a6f5f356bc9aa3d712ff5b55af
juren53 <jim.uren@gmail.com>
2023-08-22 19:38:48 -0500

    updated Tue 2023-08-22 added test for circa dates like " Ca.   Saturday, 07/01/1950"
    # updated Tue 22 Aug 2023 06:56:49 PM CDT added test for circa dates like " Ca.   Saturday, 07/01/1950"

--- Code/date-conversions.py
+++ Code/date-conversions.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-#---------------------date-conversion.py-------------------------------
-# This code converts dates from HST PDB to the IPTC Date format of YYYY-MM-DD.
+#------------date-conversion.py -- updated Tue 22 Aug 2023 06:56:49 PM CDT ----------------
+#
+# This code converts test dates from HST PDB to the ISO Date format of YYYY-MM-DD.
 # HST PDB has a _WIDE_ variety of date formats that this code tests for
 # in order for it to output a date that fits the YYY-MM-DD format
 # 
 #
 # Created Tue 27 Jun 2023 06:01:35 PM CDT by JAU
 # Updated Fri 30 Jun 2023 07:37:31 AM CDT added tests for circa dates and many others
-# Updated Mon 17 Jul 2023 04:43:58 PM CDT added test for Ca.1939 & Ca. 10/1911
+# updated Tue 22 Aug 2023 06:56:49 PM CDT added test for circa dates like " Ca.   Saturday, 07/01/1950"
 #----------------------------------------------------------------
 
 import re
@@ -16,10 +17,10 @@ from datetime import datetime
 
 def convert_date(date_str):
 
-    if date_str == 'None':  # convert NONE to 0000-00-00
+    if date_str == 'None':                      # test/convert a date like `NONE` to 0000-00-00
         return "0000-00-00"
 
-    if re.match(r'\d{4}-\d{4}', date_str):     # test for 'thru date' - YYYY - YYYY 
+    elif re.match(r'\d{4}-\d{4}', date_str):     # test/convert a date like '1900-1901' i.e.'thru date'
         year_range = date_str.split('-')
         return f"{year_range[1]}-00-00"
 
@@ -27,28 +28,15 @@ def convert_date(date_str):
         components = date_str.split('/')
         return f"20{components[2]}-{components[0]:0>2}-{components[1]:0>2}"
 
-    elif re.match(r'c\. ?\d{4}', date_str):            # test for circa date eg. c.1939
+    elif re.match(r'c\. ?\d{4}', date_str):                   # test/convert a date like `c.1939`
         year = re.findall(r'\d{4}', date_str)[0]
         return f"{year}-00-00"
 
-    elif re.match(r"ca\. ?\d{4}", date_str):            # test for circa date ca.1939
+    elif re.match(r"ca\. ?\d{4}", date_str):                  # test/convert a date like `ca.1939`
         year = date_str.split(".")[1].strip()
         return f"{year}-00-00"
 
-#########################
-
-    elif re.match(r"Ca\. ?\d{4}", date_str):            # test for circa date Ca.1939
-        year = date_str.split(".")[1].strip()
-        return f"{year}-00-00"
-
-    elif re.match(r"Ca\.  ?\d{2}/\d{4}$", date_str):  # test for circa date Ca. 10/1911
-        year_month = date_str.split(".")[1].strip()
-        month, year = year_month.split("/")
-        return f"{year}-{month:0>2}-00"
-
-#########################
-
-    elif re.match(r'\d{1,2}-[A-Za-z]{3}-\d{2}', date_str):   # test for DD-MMM-YY date
+    elif re.match(r'\d{1,2}-[A-Za-z]{3}-\d{2}', date_str):    # test/covert a date like 20-Dec-49
         try:
             date_object = datetime.strptime(date_str, "%d-%b-%y")
             year = date_object.year
@@ -59,10 +47,10 @@ def convert_date(date_str):
         except ValueError:
             return date_str
 
-    elif re.match(r'\d{4}', date_str):  # 1939
+    elif re.match(r'\d{4}', date_str):                       # test/convert a date like 1939
         return f"{date_str}-00-00"
 
-    elif re.match(r'[A-Za-z]+, \d{2}/\d{2}/\d{4}', date_str):   #test for Monday, 03/23/1964
+    elif re.match(r'[A-Za-z]+, \d{2}/\d{2}/\d{4}', date_str):   #test/covert a date like Monday, 03/23/1964
         try:
             date_object = datetime.strptime(date_str, "%A, %m/%d/%Y")
             formatted_date = date_object.strftime("%Y-%m-%d")
@@ -70,7 +58,7 @@ def convert_date(date_str):
         except ValueError:
             return date_str
 
-    elif re.match(r'[A-Za-z]{3}-\d{2}', date_str):   # test for MMM-YY date e.g. May-62
+    elif re.match(r'[A-Za-z]{3}-\d{2}', date_str):   # test/convert a date like May-62
         try:
             date_object = datetime.strptime(date_str, "%b-%y")
             year = date_object.year
@@ -81,7 +69,7 @@ def convert_date(date_str):
         except ValueError:
             return date_str
 
-    elif re.match(r'[A-Za-z]+ \d{4}', date_str):  # test for Month YYYY to YYYY-DD-00 eg. July 1948
+    elif re.match(r'[A-Za-z]+ \d{4}', date_str):  # test/convert a date like July 1948
         try:
             date_object = datetime.strptime(date_str, "%B %Y")
             formatted_date = date_object.strftime("%Y-%m-00")
@@ -89,7 +77,7 @@ def convert_date(date_str):
         except ValueError:
             return date_str
 
-    elif re.match(r'[A-Za-z]+, \d{4}', date_str):  # test for July, 1948
+    elif re.match(r'[A-Za-z]+, \d{4}', date_str):  # test/convert a date like July, 1948
         try:
             date_object = datetime.strptime(date_str, "%B, %Y")
             formatted_date = date_object.strftime("%Y-%d-00")
@@ -98,13 +86,29 @@ def convert_date(date_str):
             return date_str
 
 
-    elif re.match(r"([A-Za-z]+) (\d{1,2}), (\d{4})", date_str):  # test for September 18, 1945
+    elif re.match(r"([A-Za-z]+) (\d{1,2}), (\d{4})", date_str):  # test/convert a date like `September 18, 1945`
         try:
             date_object = datetime.strptime(date_str, "%B %d, %Y")
             formatted_date = date_object.strftime("%Y-%m-%d")
             return formatted_date
         except ValueError:
             return date_str
+
+
+    elif re.match(r"Ca\.\s+([A-Za-z]+), (\d{2})/(\d{2})/(\d{4})", date_str):   # test/convert a date like`Ca.   Saturday, 07/01/1950`
+        try:
+            match = re.match(r"Ca\.\s+([A-Za-z]+), (\d{2})/(\d{2})/(\d{4})", date_str)
+            day_name = match.group(1)
+            month = int(match.group(2))
+            day = int(match.group(3))
+            year = int(match.group(4))
+
+            date_object = datetime(year, month, day)
+            formatted_date = date_object.strftime("%Y-%m-%d")
+            return formatted_date
+        except ValueError:
+            return date_str
+
 
     return date_str
 
@@ -128,10 +132,6 @@ dates = [
     '2-Nov-59',
     '30-Sep-53',
     '1939',
-    'Ca. 1895',
-    'Ca.  10/1911',
-    'Ca.  08/1913',
-    'Ca. 1950',
     'Monday, 03/23/1964',
     'Feb-64',
     'Thursday, 07/19/1962',
@@ -142,7 +142,9 @@ dates = [
     'July 1948',
     'October 1947',
     'January, 1949',
-
+    'Ca.   Saturday, 07/01/1950',
+    'Ca.   Monday, 06/01/1953',
+    'Friday, 08/20/1948 - Sunday, 08/29/1948',
 
 ]
 
@@ -152,7 +154,7 @@ print("========      ===================")
 for date in dates:
     #print(date, "\t", convert_date(date))
 
-    print("{:<22}".format(date[:21]),\
+    print("{:<30}".format(date[:30]),\
         "{0:>10}".format(convert_date(date)))
  
 
