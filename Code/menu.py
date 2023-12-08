ec338671e0555050c618768c979a7572bb883c58
juren53 <jim.uren@gmail.com>
2023-12-08 13:30:46 -0600

    Added python check-for-files.py option 1

--- Code/menu.py
+++ Code/menu.py
@@ -4,39 +4,37 @@
 #    Front End Menu for HST-Metadata Tagging Process
 #    Updated:  Sun 22 Oct 2023 09:55:51 AM CDT 
 #    Updated:  Sun 29 Oct 2023 13:23:05 PM CDT - added replace_headers.py to menu
-#    Updated:  Tue 14 Nov 2023 10:15_01 AM CST - added copy-files.bat to "5."
+#    Updated:  Fri 08 Dec 2023 13:25:15 PM CDT - added python check-for-files.py
 #----------------------------------------------------------------------------
 
 import subprocess
 import os
 
 os.system('cls')
-print("  HST Metadata Tagging Process   [2023-11-14]")
+print("  HST Metadata Tagging Process")
 ans = True
 
 while ans:
     print ("""  
-    1. Install Python Files
-    2. Create export.csv file 
-    3. Check for valid dates
-    4. Tag TIFF or JPEG files
-    5. Move Processed TIFF and JPEG files to S: drive
-    6. Quit 
+    1  Check that necessary Python files are installed
+    2  Update headers on CSV file 
+    3  Check for valid dates
+    4  Tag TIFF or JPEG files
+
+    q  Quit 
     """)
 
-    ans = input("What would you like to do? ")
+    ans = input("  What would you like to do? ")
 
     if ans == "1":
-        subprocess.call("python install-files.py ", shell=True)
+        subprocess.call("python check-for-files.py ", shell=True)
     elif ans == "2":	
         subprocess.call("python replace_headers.py ", shell=True)
     elif ans == "3":
         subprocess.call("python check-dates-from-csv.py | more", shell=True)
     elif ans == "4":
         subprocess.call("python write-tags-from-csv.py", shell=True)
-    elif ans == "5":
-        subprocess.call("copy-files.bat", shell=True)	
-    elif ans == "6":
+    elif ans == "q":
         quit()
     else:
-        print("\n Not Valid Choice. Try again")
+        print("     Not Valid Choice. Try again")
